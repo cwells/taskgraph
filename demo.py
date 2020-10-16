ca801991bd61d7c91e00590d2fcc05cf59b8d6da
Cliff Wells <cliff.wells@gmail.com>
2020-10-15 17:58:21 -0700

    better

--- demo.py
+++ demo.py
@@ -9,45 +9,9 @@ import click
 
 from taskgraph import TaskGraph
 
-
-task = TaskGraph()
-
 class Job:
-    def __init__(self):
-        self.start_time = time.time()
+    task = TaskGraph()
 
-    def do_work(self, delay):
-        time.sleep(delay)
-        return round(time.time() - self.start_time, 1)
-
-    @task.requires()
-    def foo(self, delay):
-        return { "foo": self.do_work(delay) }
-
-    @task.requires()
-    def bar(self, delay):
-        return { "bar": self.do_work(delay) }
-
-    @task.requires(bar)
-    def baz(self, delay):
-        return { "baz": self.do_work(delay) }
-
-    @task.requires(foo, bar)
-    def qux(self, delay):
-        return { "qux": self.do_work(delay) }
-
-    @task.requires(qux, baz)
-    def quz(self, delay):
-        return { "quz": self.do_work(delay) }
-
-    @task.requires(foo, bar)
-    def xyzzy(self, delay):
-        return { "xyzzy": self.do_work(delay) }
-
-
-pipetask = TaskGraph()
-
-class JobPipeline:
     def __init__(self):
         self.start_time = time.time()
 
@@ -58,69 +22,57 @@ class JobPipeline:
             'finished': list(data.keys())
         }
 
-    @pipetask.requires()
+    @task.requires()
     def foo(self, data, delay):
-        return { "foo": self.do_work(data, delay) }
+        data.update({ "foo": self.do_work(data, delay) })
+        return data
 
-    @pipetask.requires()
+    @task.requires()
     def bar(self, data, delay):
-        return { "bar": self.do_work(data, delay) }
+        data.update({ "bar": self.do_work(data, delay) })
+        return data
 
-    @pipetask.requires(bar)
+    @task.requires(bar)
     def baz(self, data, delay):
-        return { "baz": self.do_work(data, delay) }
+        data.update({ "baz": self.do_work(data, delay) })
+        return data
 
-    @pipetask.requires(foo, bar)
+    @task.requires(foo, bar)
     def qux(self, data, delay):
-        return { "qux": self.do_work(data, delay) }
+        data.update({ "qux": self.do_work(data, delay) })
+        return data
 
-    @pipetask.requires(qux, baz)
+    @task.requires(qux, baz)
     def quz(self, data, delay):
-        return { "quz": self.do_work(data, delay) }
+        data.update({ "quz": self.do_work(data, delay) })
+        return data
 
-    @pipetask.requires(foo, bar)
+    @task.requires(foo, bar)
     def xyzzy(self, data, delay):
-        return { "xyzzy": self.do_work(data, delay) }
+        data.update({ "xyzzy": self.do_work(data, delay) })
+        return data
+
+    def run(self, data, delay):
+        return self.task.run(self, data, delay=delay)
 
 
 @click.group()
 def cli():
     pass
 
-
 @cli.command()
 @click.option('--parallel', '-p', is_flag=True, help="Run tasks in parallel.")
 @click.option('--pool-size', '-z', type=click.IntRange(1, 8), default=4, help="Size of pool for parallel processing.")
 @click.option('--delay', '-d', type=click.IntRange(0, 10), default=0, help="Seconds to sleep in functions.")
 @click.option('--graph', '-g', is_flag=True, help="Dump graph to stdout and exit (affected by -p flag).")
-def runjob(parallel, pool_size, delay, graph):
+def run(parallel, pool_size, delay, graph):
     if graph:
-        print(task.graph(parallel))
+        print(Job.task)
         raise SystemExit
 
     job = Job()
-    run = partial(task.run_parallel, pool_size=pool_size) if parallel else task.run
-    record = dict(ChainMap(*run(job, delay=delay)))
-
+    record = job.run({}, delay=delay)
     print(json.dumps(record, indent=2))
-
-
-@cli.command()
-@click.option('--parallel', '-p', is_flag=True, help="Run tasks in parallel.")
-@click.option('--pool-size', '-z', type=click.IntRange(1, 8), default=4, help="Size of pool for parallel processing.")
-@click.option('--delay', '-d', type=click.IntRange(0, 10), default=0, help="Seconds to sleep in functions.")
-@click.option('--graph', '-g', is_flag=True, help="Dump graph to stdout and exit (affected by -p flag).")
-def runpipeline(parallel, pool_size, delay, graph):
-    if graph:
-        print(pipetask.graph(parallel))
-        raise SystemExit
-
-    pipeline = JobPipeline()
-    run = partial(pipetask.pipe_parallel, pool_size=pool_size) if parallel else pipetask.pipe
-    record = run(pipeline, delay=delay)
-
-    print(json.dumps(record, indent=2))
-
 
 if __name__ == '__main__':
     cli()
--- taskgraph.py
+++ taskgraph.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 
+from copy import deepcopy
 from multiprocessing import Pool
 
 from toposort import toposort, toposort_flatten
@@ -12,69 +13,54 @@ class TaskGraph:
     '''
     def __init__(self):
         self._graph = {}
-        self._tasks = {}
+
+    def __str__(self):
+        '''Return formatted graph representation as string.
+        '''
+        sep = "\n- "
+        return sep + sep.join(', '.join(s) for s in toposort(self._graph))
+
+    def __call__(self, require=None):
+        '''Shortcut decorator for tasks with no dependencies.
+        '''
+        def wrapper(fn):
+            self._graph[fn.__name__] = set()
+            return fn
+        return wrapper
 
     def requires(self, *deps):
         '''Decorator for declaring a function as a task as well as listing
         other tasks as dependencies.
         '''
         def wrapper(fn):
-            self._graph[fn.__name__] = set(f.__name__ for f in deps or [])
-            self._tasks[fn.__name__] = fn
+            self._graph[fn.__name__] = set(f.__name__ for f in deps)
             return fn
         return wrapper
 
-    def graph(self, parallel=False):
-        '''Return formatted graph representation as string.
-        '''
-        if parallel:
-            return "(" + ") -> (".join(', '.join(s) for s in toposort(self._graph)) + ")"
-
-        return ' -> '.join([ s for s in toposort_flatten(self._graph) ])
-
-    def run(self, *args, **kwargs):
-        '''Run tasks serially.
-        Useful when:
-            - tasks share mutable state
-            - tasks are too small to be worth running in parallel
-        '''
-        for task in toposort_flatten(self._graph):
-            yield self._tasks[task](*args, **kwargs)
-
-    def run_parallel(self, *args, pool_size=4, **kwargs):
-        '''Run independent tasks in parallel.
-        For example, given dependencies a -> b and c -> d, (a, c) would be run
-        in parallel, followed by (b, d) being run in parallel.
-        '''
-        for tasks in toposort(self._graph):
-            with Pool(processes=pool_size) as pool:
-                results = []
-                for task in tasks:
-                    results.append(pool.apply_async(self._tasks[task], args, kwargs))
-                for result in results:
-                    yield result.get()
-
-    def pipe(self, *args, **kwargs):
-        '''Run tasks serially, and pass the merged return value from each task
+    def run(self, obj, data, *args, **kwargs):
+        '''Run tasks serially, and pass the return value from each task
         as the argument to the next task.
         '''
-        data = {}
         for task in toposort_flatten(self._graph):
-            data.update(self._tasks[task](*args, data=data, **kwargs))
+            try:
+                data = getattr(obj, task)(deepcopy(data), *args, **kwargs)
+            except TaskAborted:
+                return None
+
         return data
 
-    def pipe_parallel(self, *args, pool_size=4, **kwargs):
+    def run_parallel(self, obj, data, *args, pool_size=4, **kwargs):
         '''Run independent tasks in parallel, and pass the merged return values
         from each task group to the next task group.
         '''
-        data = {}
-        kwargs['data'] = data
-
         for tasks in toposort(self._graph):
             with Pool(processes=pool_size) as pool:
                 results = []
                 for task in tasks:
-                    results.append(pool.apply_async(self._tasks[task], args, kwargs))
+                    results.append(pool.apply_async(getattr(obj, task), deepcopy(data), args, kwargs))
                 for result in results:
                     data.update(result.get())
         return data
+
+class TaskAborted(Exception):
+    pass
